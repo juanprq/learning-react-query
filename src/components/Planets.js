b50533bc9fcce4ac131a8e8742c0ba1cd9571183
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-08-02 21:33:56 -0500

    adds parameters to the query

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 
 import Planet from './Planet';
 
-const fetchPlanets = async () => {
-  const response = await fetch('http://swapi.dev/api/planets');
+const fetchPlanets = async (key, greetings, page) => {
+  // we can receive the parameters of the query
+  console.log(greetings, page);
+  console.log(page);
+
+  const response = await fetch(`http://swapi.dev/api/planets?page=${page}`);
   return response.json();
 };
 
 const Planets = () => {
-  const { data, status } = useQuery(
-    'planets',
+  const [page, setPage] = useState(1);
+  const { data, status } = useQuery([
+      'planets',
+      'hello ninjas',
+      page,
+    ],
     fetchPlanets,
     {
       staleTime: 0,
@@ -22,6 +30,11 @@ const Planets = () => {
 
   return (
     <div>
+      <h2>Planets</h2>
+
+      <button onClick={() => setPage(1)}>Page 1</button>
+      <button onClick={() => setPage(2)}>Page 2</button>
+      <button onClick={() => setPage(3)}>Page 3</button>
       {
         status === 'error' && (
           <div>Error fetching data</div>
